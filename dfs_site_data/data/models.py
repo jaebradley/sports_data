0b86d9fa429bc2400fa5029a5786891831d50143
jaebradley <jae.b.bradley@gmail.com>
2016-12-28 16:22:39 -0500

    data model

--- dfs_site_data/data/models.py
+++ dfs_site_data/data/models.py
@@ -1,5 +1,138 @@
 from __future__ import unicode_literals
 
-from django.db import models
-
 # Create your models here.
+
+
+from django.db.models import Model, IntegerField, CharField, DateTimeField, ForeignKey, CASCADE
+
+
+class DfsSite(Model):
+    name = CharField(max_length=100, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Sport(Model):
+    name = CharField(max_length=100, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Position(Model):
+    sport = ForeignKey(Sport, on_delete=CASCADE, related_name='sport')
+    name = CharField(max_length=100)
+
+    class Meta:
+        unique_together = ('sport', 'name')
+
+    def __unicode__(self):
+        return '{0} - {1}'.format(self.sport, self.name)
+
+
+class DfsPosition(Model):
+    site = ForeignKey(DfsSite, on_delete=CASCADE, related_name='site')
+    position = ForeignKey(Position, on_delete=CASCADE, related_name='position')
+    site_position_id = IntegerField()
+
+    class Meta:
+        unique_together = ('site', 'position')
+
+    def __unicode__(self):
+        return '{0} - {1}'.format(self.site, self.position)
+
+
+class League(Model):
+    sport = ForeignKey(Sport, on_delete=CASCADE, related_name='sport')
+    name = CharField(max_length=100)
+
+    class Meta:
+        unique_together = ('sport', 'name')
+
+    def __unicode__(self):
+        return '{0} - {1}'.format(self.sport, self.name)
+
+
+class Team(Model):
+    league = ForeignKey(League, on_delete=CASCADE, related_name='league')
+    name = CharField(max_length=100)
+
+    class Meta:
+        unique_together = ('league', 'name')
+
+    def __unicode__(self):
+        return '{0} - {1}'.format(self.league, self.name)
+
+
+class Season(Model):
+    league = ForeignKey(League, on_delete=CASCADE, related_name='league')
+    start_time = DateTimeField()
+    end_time = DateTimeField()
+
+    class Meta:
+        unique_together = ('league', 'start_time', 'end_time')
+
+    def __unicode__(self):
+        return '{0} - {1}'.format(self.league, self.name)
+
+
+class TeamSeason(Model):
+    team = ForeignKey(Team, on_delete=CASCADE, related_name='team')
+    season = ForeignKey(Season, on_delete=CASCADE, related_name='season')
+
+    class Meta:
+        unique_together = ('team', 'season')
+
+    def __unicode__(self):
+        return '{0} - {1}'.format(self.team, self.season)
+
+
+class Player(Model):
+    team_season = ForeignKey(TeamSeason, on_delete=CASCADE, related_name='team_season')
+    name = CharField(max_length=250)
+    identifier = IntegerField()
+
+    class Meta:
+        unique_together = ('team_season', 'name')
+
+    def __unicode__(self):
+        return '{0} - {1} - {2}'.format(self.team_season, self.name, self.number)
+
+class DfsPlayer(Model):
+    player = ForeignKey(Player, on_delete=CASCADE, related_name='player')
+    site = ForeignKey(DfsSite, on_delete=CASCADE, related_name='site')
+    site_player_id = IntegerField()
+
+
+class PlayerPosition(Model):
+    player = ForeignKey(Player, on_delete=CASCADE, related_name='player')
+    position = ForeignKey(Position, on_delete=CASCADE, related_name='position')
+
+    class Meta:
+        unique_together = ('player', 'position')
+
+    def __unicode__(self):
+        return '{0} - {1}'.format(self.player, self.position)
+
+
+class Game(Model):
+    home_team = ForeignKey(Team, on_delete=CASCADE, related_name='home_team')
+    away_team = ForeignKey(Team, on_delete=CASCADE, related_name='away_team')
+    start_time = DateTimeField()
+
+    class Meta:
+        unique_together = ('home_team', 'away_team', 'start_time')
+
+    def __unicode__(self):
+        return '{0} - {1} - {2}'.format(self.home_team, self.away_team, self.start_time)
+
+
+class PlayerGame(Model):
+    player = ForeignKey(Player, on_delete=CASCADE, related_name='player')
+    game = ForeignKey(Game, on_delete=CASCADE, related_name='game')
+
+
+class DfsPlayerGame(Model):
+
+
