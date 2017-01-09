4a34cdee6769021e1d724cce7bbed5c13a44d89a
jaebradley <jae.b.bradley@gmail.com>
2017-01-08 21:53:56 -0500

    add migrations

--- dfs_site_data/settings.py
+++ dfs_site_data/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'data.apps.DataConfig',
+    'data',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
