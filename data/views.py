1c11c2471148d168228c33ab7183b5dfb863a505
jaebradley <jae.b.bradley@gmail.com>
2017-01-08 23:21:53 -0500

    add views

--- data/views.py
+++ data/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from data.models import DfsSite
+from data.serializers import DfsSiteSerializer
+
+
+class DfsSiteViewSet(ReadOnlyModelViewSet):
+    serializer_class = DfsSiteSerializer
+
+    def get_queryset(self):
+        queryset = DfsSite.objects.all().order_by('name')
+        name = self.request.query_params.get('name', None)
+        if name is not None:
+            queryset = queryset.filter(name=name)
+
+        return queryset
