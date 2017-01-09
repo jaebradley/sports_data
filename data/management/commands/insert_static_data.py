da8a24b9e5ab990ae6a83afcb6b6f2608f0f7139
jaebradley <jae.b.bradley@gmail.com>
2017-01-08 22:38:08 -0500

    add dfs inserter

---
+++ data/management/commands/insert_static_data.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+
+from data.inserters.dfs_site import DfsSiteInserter
+
+
+class Command(BaseCommand):
+    def __init__(self, stdout=None, stderr=None, no_color=False):
+        super(Command, self).__init__(stdout, stderr, no_color)
+
+    def handle(self, *args, **options):
+        Command.insert()
+
+    @staticmethod
+    def insert():
+        DfsSiteInserter.insert()
