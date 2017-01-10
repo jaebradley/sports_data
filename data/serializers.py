86dd1c0565b47a9cf78d9cd640656a8681560d42
jaebradley <jae.b.bradley@gmail.com>
2017-01-09 21:42:56 -0500

    add sport view set

--- data/serializers.py
+++ data/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import ModelSerializer
 
-from data.models import DfsSite
+from data.models import DfsSite, Sport
 
 
 class DfsSiteSerializer(ModelSerializer):
     class Meta:
         model = DfsSite()
+        fields = ('name', )
+
+
+class SportSerializer(ModelSerializer):
+    class Meta:
+        model = Sport()
         fields = ('name', )
--- data/views.py
+++ data/views.py
@@ -2,8 +2,8 @@
 
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from data.models import DfsSite
-from data.serializers import DfsSiteSerializer
+from data.models import DfsSite, Sport
+from data.serializers import DfsSiteSerializer, SportSerializer
 
 
 class DfsSiteViewSet(ReadOnlyModelViewSet):
@@ -11,6 +11,18 @@ class DfsSiteViewSet(ReadOnlyModelViewSet):
 
     def get_queryset(self):
         queryset = DfsSite.objects.all().order_by('name')
+        name = self.request.query_params.get('name', None)
+        if name is not None:
+            queryset = queryset.filter(name=name)
+
+        return queryset
+
+
+class SportViewSet(ReadOnlyModelViewSet):
+    serializer_class = SportSerializer
+
+    def get_queryset(self):
+        queryset = Sport.objects.all().order_by('name')
         name = self.request.query_params.get('name', None)
         if name is not None:
             queryset = queryset.filter(name=name)
