c5a2dffe71ee59d0fca69df1925a5f18f5ea1cb3
jaebradley <jae.b.bradley@gmail.com>
2017-01-10 01:02:05 -0500

    add inserter

--- data/inserters.py
+++ data/inserters.py
@@ -1,7 +1,7 @@
 from data.objects import DfsSite as DfsSiteObject, Sport as SportObject, League as LeagueObject, Team as TeamObject, \
-    Position as PositionObject
+    Position as PositionObject, LeaguePosition as LeaguePositionObject
 from data.models import DfsSite as DfsSiteModel, Sport as SportModel, League as LeagueModel, Team as TeamModel, \
-    Position as PositionModel
+    Position as PositionModel, LeaguePosition as LeaguePositionModel
 
 
 class DfsSite:
@@ -42,6 +42,23 @@ class League:
     @staticmethod
     def insert():
         LeagueModel.objects.bulk_create([LeagueModel(name=league.value['name'], sport=SportModel.objects.get(name=league.value['sport'].value)) for league in LeagueObject])
+
+
+class LeaguePosition:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def insert():
+        league_positions = set()
+        for league_position in LeaguePositionObject:
+            league = LeagueModel.objects.get(name=league_position.value['league'].value['name'],
+                                             sport=SportModel.objects.get(name=league_position.value['league'].value['sport'].value))
+            league_position = LeaguePosition(league=league,
+                                             position=PositionModel.objects.get(name=league_position.value['position'].value))
+            league_positions.add(league_position)
+        LeaguePositionModel.objects.bulk_create(league_positions)
 
 
 class Team:
