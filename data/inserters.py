8d53e0df334814b8dce71bff60c977bb8285caca
jaebradley <jae.b.bradley@gmail.com>
2017-01-10 22:37:58 -0500

    add draftkings league inserter

--- data/inserters.py
+++ data/inserters.py
@@ -1,7 +1,9 @@
+from draft_kings_client.draft_kings_client import Sport as DraftKingsLeague
+
 from data.objects import DfsSite as DfsSiteObject, Sport as SportObject, League as LeagueObject, Team as TeamObject, \
     Position as PositionObject, LeaguePosition as LeaguePositionObject
 from data.models import DfsSite as DfsSiteModel, Sport as SportModel, League as LeagueModel, Team as TeamModel, \
-    Position as PositionModel, LeaguePosition as LeaguePositionModel
+    Position as PositionModel, LeaguePosition as LeaguePositionModel, DfsLeague as DfsLeagueModel
 
 
 class DfsSite:
@@ -72,3 +74,19 @@ class Team:
                                                  league=LeagueModel.objects.get(name=team.value['league'].value['name'],
                                                                                 sport=SportModel.objects.get(name=team.value['league'].value['sport'].value)))
                                        for team in TeamObject])
+
+
+class DfsLeague:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def insert_draft_kings_leagues():
+        dfs_leagues = list()
+        site = DfsSiteModel.objects.get(name=DfsSiteObject.draft_kings.value)
+        for draft_kings_league in DraftKingsLeague:
+            league = LeagueModel.objects.filter(name=draft_kings_league.value.upper()).first()
+            if league is not None:
+                dfs_leagues.append(DfsLeagueModel(site=site, league=league, identifier=draft_kings_league.get_id()))
+        DfsLeagueModel.objects.bulk_create(dfs_leagues)
