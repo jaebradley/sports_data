c1640face7a19882e32daf704e99fdbd3349fd8e
jaebradley <jae.b.bradley@gmail.com>
2017-01-13 23:51:41 -0500

    update requirements

--- data/inserters/dynamic.py
+++ data/inserters/dynamic.py
@@ -1,5 +1,7 @@
 from data.models import League as LeagueModel, Team as TeamModel, Season as SeasonModel, TeamSeason as TeamSeasonModel
 
+from nba_data import Client as NbaClient, Season as NbaSeason,
+
 
 class TeamSeasonInserter:
 
@@ -19,3 +21,28 @@ class TeamSeasonInserter:
                     team_seasons.append(TeamSeasonModel(team=team, season=season))
 
         TeamSeasonModel.objects.bulk_create(team_seasons)
+
+
+class PlayersInserter:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def insert():
+        NbaPlayersInserter.insert()
+
+
+class NbaPlayersInserter:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def insert():
+        NbaClient.get_players_for_season(season=NbaSeason)
+
+
+    @staticmethod
+    def identify_start_year(year):
+        # Not dealing with any seasons so far that don't start in year x and end in year x + 1
