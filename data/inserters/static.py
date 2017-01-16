a3306eb65d2734c33f4a7769bed2196d8b5508d3
jaebradley <jae.b.bradley@gmail.com>
2017-01-15 19:33:19 -0500

    add logging for static inserters

--- data/inserters/static.py
+++ data/inserters/static.py
@@ -1,8 +1,12 @@
 from data.objects import DfsSite as DfsSiteObject, Sport as SportObject, League as LeagueObject, Team as TeamObject, \
     Position as PositionObject, LeaguePosition as LeaguePositionObject, Season as SeasonObject
 from data.models import DailyFantasySportsSite as DfsSiteModel, Sport as SportModel, League as LeagueModel, Team as TeamModel, \
-    Position as PositionModel, LeaguePosition as LeaguePositionModel, Season as SeasonModel, \
-    TeamSeason as TeamSeasonModel
+    Position as PositionModel, LeaguePosition as LeaguePositionModel, Season as SeasonModel
+
+import logging
+
+
+logging.basicConfig(filename='static-inserter.log', level=logging.INFO)
 
 
 class DfsSiteInserter:
@@ -12,6 +16,7 @@ class DfsSiteInserter:
 
     @staticmethod
     def insert():
+        logging.info('Inserting DFS sites')
         DfsSiteModel.objects.bulk_create([DfsSiteModel(name=site.value) for site in DfsSiteObject])
 
 
@@ -22,6 +27,7 @@ class SportInserter:
 
     @staticmethod
     def insert():
+        logging.info('Inserting sports')
         SportModel.objects.bulk_create([SportModel(name=sport.value) for sport in SportObject])
 
 
@@ -32,6 +38,7 @@ class PositionInserter:
 
     @staticmethod
     def insert():
+        logging.info('Inserting positions')
         PositionModel.objects.bulk_create([PositionModel(name=position.value) for position in PositionObject])
 
 
@@ -42,6 +49,7 @@ class LeagueInserter:
 
     @staticmethod
     def insert():
+        logging.info('Inserting leagues')
         LeagueModel.objects.bulk_create([LeagueModel(name=league.value['name'], sport=SportModel.objects.get(name=league.value['sport'].value)) for league in LeagueObject])
 
 
@@ -52,12 +60,21 @@ class LeaguePositionInserter:
 
     @staticmethod
     def insert():
+        logging.info('Inserting league positions')
         league_positions = list()
         for league_position in LeaguePositionObject:
+            logging.info('Inserting league position: %s' % league_position)
+
             sport = SportModel.objects.get(name=league_position.value['league'].value['sport'].value)
+            logging.info('Fetching sport: %s' % sport)
+
             position = PositionModel.objects.get(name=league_position.value['position'].value)
+            logging.info('Fetching position: %s' % position)
+
             league = LeagueModel.objects.get(name=league_position.value['league'].value['name'],
                                              sport=sport)
+            logging.info('Fetching league: %s' % league)
+
             league_positions.append(LeaguePositionModel(league=league, position=position))
         LeaguePositionModel.objects.bulk_create(league_positions)
 
@@ -69,6 +86,7 @@ class TeamInserter:
 
     @staticmethod
     def insert():
+        logging.info('Inserting teams')
         TeamModel.objects.bulk_create([TeamModel(name=team.value['name'],
                                                  league=LeagueModel.objects.get(name=team.value['league'].value['name'],
                                                                                 sport=SportModel.objects.get(name=team.value['league'].value['sport'].value)))
@@ -82,8 +100,12 @@ class SeasonInserter:
 
     @staticmethod
     def insert():
+        logging.info('Inserting seasons')
         seasons = list()
         for season in SeasonObject:
+            logging.info('Inserting season: %s' % season)
             league = LeagueModel.objects.get(name=season.value['league'].value['name'])
+
+            logging.info('Fetching league: %s' % league)
             seasons.append(SeasonModel(league=league, start_time=season.value['start_time'], end_time=season.value['end_time']))
         SeasonModel.objects.bulk_create(seasons)
