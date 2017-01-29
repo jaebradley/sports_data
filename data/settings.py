c6d3f93f6104c94d90aa0a250aebc9ae7d190efe
jaebradley <jae.b.bradley@gmail.com>
2017-01-29 15:09:11 -0500

    add migrations and settings

--- data/settings.py
+++ data/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'data',
+    'dfs_site_persistence',
+    'nba_persistence',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
