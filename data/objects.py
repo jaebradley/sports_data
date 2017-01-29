d16eb2c9dd3ab70b3b8691e3fce0e96adb9f1e4e
jaebradley <jae.b.bradley@gmail.com>
2017-01-29 14:47:35 -0500

    move dfs enum

--- data/objects.py
+++ data/objects.py
@@ -3,21 +3,6 @@ import datetime
 import pytz
 
 
-class DfsSite(Enum):
-    draft_kings = 'DraftKings'
-    fan_duel = 'FanDuel'
-
-    @staticmethod
-    def value_of(name):
-        assert isinstance(name, basestring)
-
-        for site in DfsSite:
-            if site.value == name.upper():
-                return site
-
-        raise ValueError('Unable to identify site with name: %s', site)
-
-
 class Sport(Enum):
     basketball = 'Basketball'
     football = 'Football'
