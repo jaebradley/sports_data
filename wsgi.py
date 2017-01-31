a6e007d05021a7603e2e6ad7de1348a5fce98b96
jaebradley <jae.b.bradley@gmail.com>
2017-01-31 02:00:11 -0500

    update

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,11 @@
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 from whitenoise.django import DjangoWhiteNoise
+import os
+import dotenv
+
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+
 
 application = DjangoWhiteNoise(get_wsgi_application())
 application = Cling(get_wsgi_application())
