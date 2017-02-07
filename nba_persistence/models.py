62bbeb1b3cf423c9950dd6840c4689a14a735ca0
jaebradley <jae.b.bradley@gmail.com>
2017-02-07 00:38:08 -0500

    add nba box score

--- nba_persistence/models.py
+++ nba_persistence/models.py
@@ -1,5 +1,29 @@
 from __future__ import unicode_literals
 
-from django.db import models
+from django.db.models import Model, BigIntegerField, CharField, ForeignKey, \
+    CASCADE
 
-# Create your models here.
+from data.models import GamePlayer
+
+
+class BoxScore(Model):
+    game_player = ForeignKey(GamePlayer, on_delete=CASCADE)
+    status = CharField(max_length=100, default=None)
+    explanation = CharField(max_length=100, default=None)
+    seconds_played = BigIntegerField(null=False, default=0),
+    field_goals_made = BigIntegerField(null=False, default=0),
+    field_goals_attempted = BigIntegerField(null=False, default=0),
+    three_point_field_goals_made = BigIntegerField(null=False, default=0),
+    three_point_field_goals_attempted = BigIntegerField(null=False, default=0),
+    free_throws_made = BigIntegerField(null=False, default=0),
+    free_throws_attempted = BigIntegerField(null=False, default=0),
+    offensive_rebounds = BigIntegerField(null=False, default=0),
+    defensive_rebounds = BigIntegerField(null=False, default=0),
+    total_rebounds = BigIntegerField(null=False, default=0),
+    assists = BigIntegerField(null=False, default=0),
+    steals = BigIntegerField(null=False, default=0),
+    blocks = BigIntegerField(null=False, default=0),
+    turnovers = BigIntegerField(null=False, default=0),
+    personal_fouls = BigIntegerField(null=False, default=0),
+    points = BigIntegerField(null=False, default=0),
+    plus_minus = BigIntegerField(null=False, default=0),
